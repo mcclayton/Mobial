a160be82ab8ccd3f0655e12e127d355b7e4245a5
Michael Clayton <mostly_mike@hotmail.com>
2014-01-16 23:49:16 -0500

    Added drag logging.

--- Android/src/com/mhacks/mobial/Mobial.java
+++ Android/src/com/mhacks/mobial/Mobial.java
@@ -4,15 +4,26 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class Mobial {
-	// TODO Create a super awesome library of functions that collect and mobile analytics data.
 	
 	public static void startTrackingGestures(View v) {
+		
 		v.setOnTouchListener(new View.OnTouchListener() {
-			
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				System.out.println("X: "+event.getX()+"   Y:"+event.getY());
-				return false;
+			    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			    	// The user started dragging
+			    	System.out.println("START!");
+			    	return true;
+			    } else if(event.getAction() == MotionEvent.ACTION_MOVE) {
+			    	// The user is likely dragging
+			    	System.out.println("X: "+event.getX()+"   Y:"+event.getY());
+					return true;
+			    } else if (event.getAction() == MotionEvent.ACTION_UP) {
+			    	// The user is done dragging
+			    	System.out.println("END!");
+			    	return true;
+			    }
+			    return true;
 			}
 		});
 	}
