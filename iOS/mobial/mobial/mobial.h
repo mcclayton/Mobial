ec572bc21ddc37d5d885199fab8e6a94c6cbbe00
Vipul Nataraj <vnataraj@purdue.edu>
2014-01-16 22:30:00 -0500

    Added dummy iOS static library

---
+++ iOS/mobial/mobial/mobial.h
@@ -0,0 +1,13 @@
+//
+//  mobial.h
+//  mobial
+//
+//  Created by Vipul Nataraj on 1/16/14.
+//  Copyright (c) 2014 espressbros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mobial : NSObject
+
+@end
---
+++ iOS/mobial/mobial/mobial.m
@@ -0,0 +1,13 @@
+//
+//  mobial.m
+//  mobial
+//
+//  Created by Vipul Nataraj on 1/16/14.
+//  Copyright (c) 2014 espressbros. All rights reserved.
+//
+
+#import "mobial.h"
+
+@implementation mobial
+
+@end
