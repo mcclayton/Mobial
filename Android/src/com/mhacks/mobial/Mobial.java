20243bb725b6eb7967831efa0959133f7dec18af
Michael Clayton <mostly_mike@hotmail.com>
2014-01-17 23:16:10 -0500

    Added screenshot to activity flow JSON.

--- Android/src/com/mhacks/mobial/Mobial.java
+++ Android/src/com/mhacks/mobial/Mobial.java
@@ -1,11 +1,14 @@
 package com.mhacks.mobial;
 
+import java.io.ByteArrayOutputStream;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.content.ComponentName;
+import android.graphics.Bitmap;
+import android.util.Base64;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -16,21 +19,25 @@ public class Mobial {
 	private static JSONArray activityFlowJSONArray = new JSONArray();
 	private static JSONArray touchCoordsJSONArray = new JSONArray();
 
-
 	public static void startTrackingActivityFlow(Activity act) {		
 		JSONObject activityJSON = new JSONObject();
 
-		try {			
-			activityJSON.put("activityName", act.getLocalClassName());			
+		try {	
+			
+			activityJSON.put("activityName", act.getLocalClassName());	// Add the current activity name to the JSON
+			
+			// Take a screenshot, encode it, and add the encoded screenshot to the JSON
+			Bitmap screenshot = takeScreenShot(act);
+			activityJSON.put("activityScreenshot", bitmapToString(screenshot));
 			activityFlowJSONArray.put(activityJSON);
 
 			System.out.println(activityFlowJSONArray.toString());
 			//TODO: Make the POST request to the server here.
-			
+
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}		
 	}
 
 	public static void startTrackingGestures(Activity act) {
@@ -62,10 +69,10 @@ public class Mobial {
 						touchCoordJSON.put("y_coord", event.getY());
 						touchCoordJSON.put("state", "end_drag");
 						touchCoordsJSONArray.put(touchCoordJSON);
-						
+
 						System.out.println(touchCoordsJSONArray.toString());
 						//TODO: Make the POST request to the server here.
-						
+
 						return true;
 					}
 				} catch (JSONException e) {
@@ -76,6 +83,30 @@ public class Mobial {
 				return true;
 			}
 		});
+	}
+	
+	private static Bitmap takeScreenShot(Activity act) {
+		//View view = (View) act.findViewById(android.R.id.content);
+		View view = act.getWindow().getDecorView();
+		Bitmap bitmap;
+		view.setDrawingCacheEnabled(true);
+		bitmap = Bitmap.createBitmap(view.getDrawingCache());
+		view.setDrawingCacheEnabled(false);
+		return bitmap;
+	}
+
+	/**
+	 * Encodes a Bitmap picture to a string which can be JSONified.
+	**/
+	private static String bitmapToString(Bitmap bitmapPicture) {
+		final int COMPRESSION_QUALITY = 100;
+		String encodedImage;
+		ByteArrayOutputStream byteArrayBitmapStream = new ByteArrayOutputStream();
+		bitmapPicture.compress(Bitmap.CompressFormat.PNG, COMPRESSION_QUALITY,
+				byteArrayBitmapStream);
+		byte[] b = byteArrayBitmapStream.toByteArray();
+		encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+		return encodedImage;
 	}
 
 }
